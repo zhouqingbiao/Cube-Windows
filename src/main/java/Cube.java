787409a1e6ffc688d0932495d0ca0b7c1562cb9e
zhouqingbiao <zhouqingbiao@qq.com>
2024-10-18 16:57:00 +0800

    重构一下 让代码更难读 也许

--- src/main/java/Cube.java
+++ src/main/java/Cube.java
@@ -1,157 +1,127 @@
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-import java.util.Random;
+import java.io.File;
+import java.util.List;
+import java.util.*;
 
 public class Cube extends JFrame {
     /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+     *
+     */
+    private static final long serialVersionUID = 1L;
 
-	// JLabel在此定义
-    // 须重复使用
     JLabel label = new JLabel("", JLabel.CENTER);
 
-    // 窗口标题
     String title;
 
-    // 根目录
-    String root;
+    int state = 1;
 
-    // 二级CFOP目录
-    String f2l;
-    String oll;
-    String pll;
+    Map<Integer, String> map = new HashMap<>();
 
-    // CFOP状态
-    String cfop;
-
-    // CFOP图片名称
-    String[] f2ls;
-    String[] olls;
-    String[] plls;
+    List<String> listOfOLL = new ArrayList<>();
+    List<String> listOfPLL = new ArrayList<>();
+    List<String> listOfF2L = new ArrayList<>();
 
     {
-        // 获取Properties数据
-        Properties properties = new Properties();
-        try {
-            properties.load(this.getClass().getClassLoader().getResourceAsStream("Cube.properties"));
-            // 窗口标题
-            title = properties.getProperty("title");
 
-            // 根目录
-            root = properties.getProperty("root");
+        map.put(1, "OLL");
+        map.put(2, "PLL");
+        map.put(3, "F2L");
 
-            // 二级CFOP目录
-            f2l = properties.getProperty("F2L");
-            oll = properties.getProperty("OLL");
-            pll = properties.getProperty("PLL");
-
-            // CFOP图片名称
-            f2ls = properties.getProperty("F2Ls").split("#");
-            olls = properties.getProperty("OLLs").split("#");
-            plls = properties.getProperty("PLLs").split("#");
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        File file;
+        for (int i = 0; i < map.size(); i++) {
+            file = new File(Objects.requireNonNull(Cube.class.getResource("")).getPath() + File.separator + map.get(i + 1));
+            for (File f : Objects.requireNonNull(file.listFiles())) {
+                if (f.isFile()) {
+                    if (i == 0) {
+                        listOfOLL.add(f.getAbsolutePath());
+                    }
+                    if (i == 1) {
+                        listOfPLL.add(f.getAbsolutePath());
+                    }
+                    if (i == 2) {
+                        listOfF2L.add(f.getAbsolutePath());
+                    }
+                }
+            }
         }
     }
 
     public void init() {
-        // 使用BorderLayout方便JLabel水平垂直居中
         JPanel panel = new JPanel(new BorderLayout());
 
-        // 居中添加JLabel
-        panel.add(initJLabel(cfop), BorderLayout.CENTER);
+        panel.add(insertIcon(state), BorderLayout.CENTER);
 
-        // 添加JPanel
         this.getContentPane().add(panel);
 
-        // 添加键盘事件（空格键随机切换OLL/PLL，左键OLL，右键PLL。）
         this.addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
                 super.keyPressed(e);
-                // 空格键
                 if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-                    initJLabel(cfop);
+                    insertIcon(state);
                 }
-                // 左键
                 if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-                    cfop = oll;
-                    initJLabel(cfop);
+                    state = 1;
+                    insertIcon(state);
                 }
-                // 右键
                 if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-                    cfop = pll;
-                    initJLabel(cfop);
+                    state = 2;
+                    insertIcon(state);
+                }
+                if (e.getKeyCode() == KeyEvent.VK_UP) {
+                    state = 3;
+                    insertIcon(state);
                 }
             }
         });
 
-        // 设置窗口标题
         this.setTitle(title);
-        // 设置窗口大小
         this.setSize(400, 400);
-        // JFrame居中显示
         this.setLocationRelativeTo(null);
-        // 能够点击关闭窗口
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // 窗口可见
         this.setVisible(true);
 
     }
 
-    /**
-     * 初始化JLabel，即随机切换OLL/PLL。
-     */
-    public JLabel initJLabel(String cfop) {
-        ImageIcon ii = null;
-        try {
-            ii = new ImageIcon(ImageIO.read(path(cfop)));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        // 放大图片，但是放大后失真，不知如何处理。
-        // oll放大不失真，pll放大失真。
-        if (oll.equals(cfop) || null == cfop) {
-            // 断言
-            assert ii != null;
-            ii.setImage(ii.getImage().getScaledInstance(128, 128, Image.SCALE_DEFAULT));
-        }
-        // 设置图片
+    public JLabel insertIcon(int state) {
+        ImageIcon ii = new ImageIcon(getAbsolutePath(state));
         label.setIcon(ii);
         return label;
     }
 
-    /**
-     * 获取OLL/PLL路径。
-     */
-    public InputStream path(String cfop) {
-        // 随机
+    public String getAbsolutePath(int state) {
         Random random = new Random();
 
-        // 组装路径
-        String name;
-        if (f2l.equals(cfop)) {
-            name = f2l + "/" + f2ls[(random.nextInt(f2ls.length))];
-        } else if (oll.equals(cfop)) {
-            name = oll + "/" + olls[(random.nextInt(olls.length))];
-        } else if (pll.equals(cfop)) {
-            name = pll + "/" + plls[(random.nextInt(plls.length))];
-        } else {
-            name = oll + "/" + olls[(random.nextInt(olls.length))];
-        }
-        name = root + "/" + name;
+        String name = "";
 
-        // getResourceAsStream可以获取流
-        return this.getClass().getResourceAsStream(name);
+        int i;
+
+        if (state == 1) {
+            if (!listOfOLL.isEmpty()) {
+                i = random.nextInt(listOfOLL.size());
+                name = listOfOLL.get(i);
+                listOfOLL.remove(i);
+            }
+        }
+        if (state == 2) {
+            if (!listOfPLL.isEmpty()) {
+                i = random.nextInt(listOfPLL.size());
+                name = listOfPLL.get(i);
+                listOfPLL.remove(i);
+            }
+        }
+        if (state == 3) {
+            if (!listOfF2L.isEmpty()) {
+                i = random.nextInt(listOfF2L.size());
+                name = listOfF2L.get(i);
+                listOfF2L.remove(i);
+            }
+        }
+
+        return name;
     }
 
     public static void main(String[] args) {
